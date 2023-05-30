a89f733cc1118871a5369a307947bf12115ed96b
olegdoronenko <61374593+olegdoronenko@users.noreply.github.com>
2023-05-30 21:03:06 +0300

    Initial commit

---
+++ src/main/java/KataPP/PlayersTable/PlayersTableApplication.java
@@ -0,0 +1,13 @@
+package KataPP.PlayersTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KataPP/PlayersTable/PlayersTableApplicationTests.java
@@ -0,0 +1,13 @@
+package KataPP.PlayersTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
